2a3319ebba3a272e74063518ab61a1f6c6364a65
snezamha <snezamha@gmail.com>
2023-02-22 01:54:41 +0100

    Add swagger in /api url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
     origin: '',
     credentials: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('MyCMS')
+    .setDescription('The MyCMS API helper')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
